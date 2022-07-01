68e1b41021c5a5816c254a06aaa712d5dee9179c
cristianbad <cristianbad.rau@gmail.com>
2022-07-01 04:34:38 -0700

    cached2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
@@ -27,5 +28,6 @@ class AppServiceProvider extends ServiceProvider
     {
         Model::unguard();
         //Paginator::useBootstrap();
+        URL::forceScheme('https');
     }
 }
